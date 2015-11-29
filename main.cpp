dcc3573d91f0ef64b9152530f97f149147811f29
webserg@gmail.com <webserg@gmail.com>
2015-11-29 12:41:44 +0200

    'add'

---
+++ main.cpp
@@ -0,0 +1,113 @@
+#include <SFML/Graphics.hpp>
+class Game
+{
+public:
+	Game();
+	void run();
+private:
+	void processEvents();
+	void update(sf::Time deltaTime);
+	void render();
+	void handlePlayerInput(sf::Keyboard::Key key, bool isPressed);
+private:
+	sf::RenderWindow mWindow;
+	sf::CircleShape mPlayer;
+	bool mIsMovingUp;
+	bool mIsMovingDown;
+	bool mIsMovingLeft;
+	bool mIsMovingRight;
+	float PlayerSpeed = 0.5f;
+};
+
+Game::Game()
+	: mWindow(sf::VideoMode(640, 480), "SFML Application")
+	, mPlayer()
+{
+	mPlayer.setRadius(40.f);
+	mPlayer.setPosition(100.f, 100.f);
+	mPlayer.setFillColor(sf::Color::Cyan);
+}
+void Game::run()
+{
+	sf::Clock clock;
+	while (mWindow.isOpen())
+	{
+		sf::Time deltaTime = clock.restart();
+		processEvents();
+		update(deltaTime);
+		render();
+	}
+}
+void Game::processEvents()
+{
+	sf::Event event;
+	while (mWindow.pollEvent(event))
+	{
+		switch (event.type)
+		{
+		case sf::Event::KeyPressed:
+			handlePlayerInput(event.key.code, true);
+			break;
+		case sf::Event::KeyReleased:
+			handlePlayerInput(event.key.code, false);
+			break;
+		case sf::Event::Closed:
+			mWindow.close();
+			break;
+		}
+	}	
+}
+void Game::handlePlayerInput(sf::Keyboard::Key key,	bool isPressed)
+{
+	if (key == sf::Keyboard::W)
+		mIsMovingUp = isPressed;
+	else if (key == sf::Keyboard::S)
+		mIsMovingDown = isPressed;
+	else if (key == sf::Keyboard::A)
+		mIsMovingLeft = isPressed;
+	else if (key == sf::Keyboard::D)
+		mIsMovingRight = isPressed;
+}
+void Game::update(sf::Time deltaTime)
+{
+	sf::Vector2f movement(0.f, 0.f);
+	if (mIsMovingUp)
+		movement.y -= PlayerSpeed;
+	if (mIsMovingDown)
+		movement.y += PlayerSpeed;
+	if (mIsMovingLeft)
+		movement.x -= PlayerSpeed;
+	if (mIsMovingRight)
+		movement.x += PlayerSpeed;
+	mPlayer.move(movement * deltaTime.asSeconds());
+}
+void Game::render()
+{
+	mWindow.clear();
+	mWindow.draw(mPlayer);
+	mWindow.display();
+}
+int main()
+{
+	Game game;
+	game.run();
+	/*sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+	*/
+	return 0;
+}
